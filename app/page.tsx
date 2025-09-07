ebd7c6bff949b37a6a40e097bd92b2ba98e829a2
Kanak Joshi <joshi31.kanak@gmail.com>
2025-09-07 23:02:00 +0530

    feat: add inquiry form ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from '@/components/ui/button'
+'use client'
+import TravelInquiryForm from '@/components/InquiryForm/travelInquiry'
 
 export default function Home() {
 	return (
 		<main className="bg-background text-foreground grid min-h-screen place-items-center">
-			<h1 className="text-accent font-sans text-4xl font-semibold tracking-tight sm:text-6xl">
-				kidu kerela 🌴
-			</h1>
-			<Button variant="outline">Click me</Button>
+			<TravelInquiryForm />
 		</main>
 	)
 }
