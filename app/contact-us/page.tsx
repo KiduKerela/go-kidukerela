2381218c3028817b47696de12a6067d2792e40b3
Kanak Joshi <joshi31.kanak@gmail.com>
2025-10-09 10:42:30 +0530

    feat: redesign homepage layout and add contact us page

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import TravelInquiryForm from '@/components/InquiryForm/travelInquiry'
+
+export default function Home() {
+	return (
+		<main className="bg-background text-foreground grid min-h-screen place-items-center">
+			<TravelInquiryForm />
+		</main>
+	)
+}
